a31416f228685d3bee702dbcb86b3c8201e045da
Henrique Cavalieri <henrique@kaordica.com.br>
2019-05-01 16:36:07 -0300

    Removing sanity-scaffold; Migrating types and mock-data to /kommunity

--- sanity-scaffold/schemas/stringOptions.js
+++
@@ -1,4 +0,0 @@
-export const ctaTypes = [
-  { title: 'Caixa centralizada (sem imagem)', value: 'centeredBox' },
-  { title: 'Imagem a esquerda', value: 'imgLeft' }
-]
--- types/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './src/submitRec'
-export * from './src/contentCard'
-export * from './src/curatedContent'
